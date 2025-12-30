c752e7d3bac21cf013caddae838380f46c83cf70
utujesandrine456 <utujesandrine456@gmail.com>
2025-12-30 21:15:56 +0200

    Genzura Setup Project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>BuildSmart RW</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="bg-gray-100 font-sans">
+    <div class="container mx-auto p-6">
+        <h1 class="text-4xl font-bold text-center text-blue-600">Welcome to BuildSmart RW</h1>
+        <p class="text-center mt-4">Manage construction projects smarter.</p>
+    </div>
+</body>
+</html>
